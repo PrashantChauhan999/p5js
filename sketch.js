a525de920f448990ef7f91b1f8c34ae87d878645
PrashantChauhan999 <prashantchauhan09@gmail.com>
2024-07-19 15:58:51 +0530

    create boundary rectangle box to stand inside box and only pose detection start

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let video;
 let poseNet;
 let poses = [];
+let skeleton;
 
 let url = new URL(window.location.href);
 let params = new URLSearchParams(url.search);
@@ -11,7 +12,7 @@ let enableSkeleton = params.get("enableSkeleton") || true;
 let enableKeyPoints = params.get("enableKeyPoints") || true;
 let color = params.get("color") || "255, 0, 255";
 let mode = params.get("mode") || "multiple";
-let score = params.get("score") || 0.5;
+let score = params.get("score") || 0.2;
 let isBackCamera = params.get("isBackCamera") || false;
 let flipHorizontal = params.get("flipHorizontal") || false;
 let RR = color.split(",")[0];
@@ -20,33 +21,12 @@ let BB = color.split(",")[2];
 
 let isFullScreen = params.get("isFullScreen") || true;
 
-
-// function setup() {
-//   createCanvas(width, height);
-//   video = createCapture({
-//     audio: false,
-//     video: {
-//       facingMode: isBackCamera ? "environment" : "user",
-//       width: width,
-//       height: height,
-//     },
-//   });
-//   video.size(width, height);
-
-//   // Create a new poseNet method with a single detection
-//   poseNet = ml5.poseNet(video, mode, modelReady);
-//   // This sets up an event that fills the global variable "poses"
-//   // with an array every time new poses are detected
-//   poseNet.on("pose", function (results) {
-//     poses = results;
-//     window?.ReactNativeWebView?.postMessage(JSON.stringify(poses));
-//     // console.log(poses);
-//   });
-//   // Hide the video element, and just show the canvas
-//   video.hide();
-// }
-
-
+let boundaryBox = {
+  x: 100,
+  y: 100,
+  w: 500,
+  h: 800,
+};
 
 function setup() {
   let canvasWidth = isFullScreen ? width : windowWidth;
@@ -78,21 +58,20 @@ function windowResized() {
   let canvasHeight = isFullScreen ? height : windowHeight;
   resizeCanvas(canvasWidth, canvasHeight);
   video.size(canvasWidth, canvasHeight);
-  if(isFullScreen){
+  if (isFullScreen) {
     document.getElementById("defaultCanvas0").style.height = "100vh";
     document.getElementById("defaultCanvas0").style.width = "100vw";
   }
 }
 
-
-
-
 function modelReady() {
   console.log("Model Ready");
-  if(isFullScreen){
+  if (isFullScreen) {
     document.getElementById("defaultCanvas0").style.height = "100vh";
     document.getElementById("defaultCanvas0").style.width = "100vw";
-  } 
+  } else {
+    console.log("Full visible in camera");
+  }
 }
 
 function draw() {
@@ -104,24 +83,53 @@ function draw() {
   }
   image(video, 0, 0, canvasWidth, canvasHeight);
 
-  // We can call both functions to draw all keypoints and the skeletons
-  if (enableKeyPoints !== 'false') drawKeypoints();
-  if (enableSkeleton !== 'false') drawSkeleton();
+  let allInside = true;
+  let allKeyPointsVisible = false;
+
+  for (let i = 0; i < poses.length; i++) {
+    const pose = poses[i].pose;
+    let keypointsVisibleCount = 0;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      const keypoint = pose.keypoints[j];
+      if (keypoint.score > score) {
+        keypointsVisibleCount++;
+        if (
+          keypoint.position.x < boundaryBox.x ||
+          keypoint.position.x > boundaryBox.x + boundaryBox.w ||
+          keypoint.position.y < boundaryBox.y ||
+          keypoint.position.y > boundaryBox.y + boundaryBox.h
+        ) {
+          allInside = false;
+        }
+      }
+    }
+    if (keypointsVisibleCount === 17) {
+      allKeyPointsVisible = true;
+    }
+  }
+
+  if (allInside) {
+    stroke(117, 253, 113); // Green if all keypoints are inside
+  } else {
+    stroke(255, 0, 0); // Red if any keypoint is outside
+  }
+  noFill();
+  rect(boundaryBox.x, boundaryBox.y, boundaryBox.w, boundaryBox.h);
+
+  // Draw keypoints and skeleton only if all 17 keypoints are visible
+  if (allKeyPointsVisible) {
+    if (enableKeyPoints !== "false") drawKeypoints();
+    if (enableSkeleton !== "false") drawSkeleton();
+  }
 }
 
-// A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
-  // Loop through all the poses detected
-  // console.log(poses);
-  for (let i = 0; i < poses.length; i += 1) {
-    // For each pose detected, loop through all the keypoints
+  for (let i = 0; i < poses.length; i++) {
     const pose = poses[i].pose;
-    for (let j = 0; j < pose.keypoints.length; j += 1) {
-      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+    for (let j = 0; j < pose.keypoints.length; j++) {
       const keypoint = pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > score) {
-        fill(RR, GG, BB);
+        fill(RR, 0, 0);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
       }
@@ -129,16 +137,13 @@ function drawKeypoints() {
   }
 }
 
-// A function to draw the skeletons
 function drawSkeleton() {
-  // Loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i += 1) {
+  for (let i = 0; i < poses.length; i++) {
     const skeleton = poses[i].skeleton;
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < skeleton.length; j += 1) {
+    for (let j = 0; j < skeleton.length; j++) {
       const partA = skeleton[j][0];
       const partB = skeleton[j][1];
-      stroke(RR, GG, BB);
+      stroke(RR, 0, 0);
       line(
         partA.position.x,
         partA.position.y,
@@ -148,5 +153,3 @@ function drawSkeleton() {
     }
   }
 }
-
-
